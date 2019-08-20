33d68c6ea12cf54dc4f0c1eb7ee2e86965e2acee
Giorgio Zett <giorgio.zett@gmail.com>
2019-08-20 13:55:04 +0200

    update admin hashed pwd in db init sql script

--- backend/db/2-data.sql
+++ backend/db/2-data.sql
@@ -1,4 +1,4 @@
 INSERT INTO `users`(`name`, `surname`, `email`, `gender`) VALUES ('admin', 'admin', 'admin@example.com', 'M');
-INSERT INTO `credentials`(`userId`, `username`, `password`, `role`) VALUES ((select id from users where email='admin@example.com'), 'admin', 'admin', 'A');
+INSERT INTO `credentials`(`userId`, `username`, `password`, `role`) VALUES ((select id from users where email='admin@example.com'), 'admin', '$argon2i$v=19$m=65536,t=4,p=1$U2gyb0pzamYyQy96cGdhRg$kqmoNb5rVLm3+0bFs2t4yJSvhOuoRzjVNY8pOrL62GU', 'A');
 
 INSERT INTO `users`(`name`, `surname`, `email`, `gender`) VALUES ('user', 'user', 'user@example.com', 'M')
